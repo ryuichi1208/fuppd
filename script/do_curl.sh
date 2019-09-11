359c61cb08b0855cebb526a304f29997a60d2a63
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-11 20:37:33 +0900

    add access check

--- script/do_curl.sh
+++ script/do_curl.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+CURL=$(which curl)
+CURL_OPT="-sSLI"
+
+FLASK_IPADDR=
+FLASK_PORT=$(cat settings/flask.ini | grep "FLASK_PORT" | cut -d = -f 2)
+
+ENDPOINT=( ""
+           "__health"
+           "__count"
+           "__notfound"
+)
+
+for EP in "${ENDPOINT[@]}"
+do
+  ${CURL} ${CURL_OPT} http://localhost:${FLASK_PORT}/${EP} -o /dev/null -w '%{http_code}\n'
+done
